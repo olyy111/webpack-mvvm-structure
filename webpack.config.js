5cbf8ba24b3e201b3bbe4ad41ed80be16df715aa
zhangliang7 <zl7511555@gmail.com>
2019-05-21 11:27:43 +0800

    配置publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist/'),
         filename: 'static/js/[name].js',
+        publicPath: '/'
     },
     optimization: {
         minimizer: [new OptimizeCSSAssetsPlugin()]
