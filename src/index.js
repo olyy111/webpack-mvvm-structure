c6edb178629a9ed24f42fd43526d1b847d529d85
zhangliang7 <zl7511555@gmail.com>
2019-06-05 10:50:41 +0800

    添加sass解析

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import './index.scss'
 
 class App extends React.Component {
   render() {
-    return <h2 className="title">hello， 这是一个react应用</h2>
+    
+     
+    return <div>
+     <h2 className="title">hello， 这是一个react应用</h2>
+     <p class="content">react 是一个mvvm 框架</p>
+    </div>
   }
 }
 ReactDOM.render(
