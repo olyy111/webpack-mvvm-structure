d4f3b3eef4186eccea35831d40436fe9a3426b5e
zhangliang7 <zl7511555@gmail.com>
2019-06-06 12:18:09 +0800

    清楚无用的dev编译信息, 并且添加一个成功提示

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,7 +1,18 @@
 var  merge = require('webpack-merge')
 var baseConfig =  require('./webpack.config.base.js')
+var FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+var port = 6611
 console.log('----dev-----')
 module.exports = merge(baseConfig, {
   devServer: {
-  }
+    port,
+    quiet: true // 清楚dev编译无用的console信息
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin({ // 添加成功提示和自定义信息
+      compilationSuccessInfo: {
+        messages: [`You application is running here http://localhost:${port}`]
+      },
+    }),
+  ],
 })
