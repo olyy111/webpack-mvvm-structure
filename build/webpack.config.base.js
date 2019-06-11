394b10b4f46b691743b9cd2161d7cdd252b04119
张良 <zhangliang7@ucfgroup.com>
2019-06-11 15:54:03 +0800

    添加typescript

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -4,13 +4,18 @@ console.log(1111, __dirname)
 module.exports = {
   mode: 'none',
   entry: {
-    app: path.resolve(__dirname, '../src/index.js')
+    app: path.resolve(__dirname, '../src/index.tsx')
   },
-  output: {
-    filename: '[name].js'
-  },
+  
   module: {
     rules: [
+      { test: /\.(tsx|ts)?$/, 
+        loader: "ts-loader",
+        options: {
+          configFile: '../tsconfig.js',
+          transpileOnly: true
+        }
+      },
       {
         test: /\.js$/,
         loader: 'babel-loader'
